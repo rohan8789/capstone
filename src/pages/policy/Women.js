1b310dda01d071c3a9243087ee9a37fcd656a51a
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:10:57 +0530

    Update Women.js

--- src/pages/policy/Women.js
+++ src/pages/policy/Women.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Women = () => {
   return (
-    <div>Women</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Women related policies</h2>
+    </div>
   )
 }
 
