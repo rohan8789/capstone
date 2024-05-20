96a82bc1029acf7178c0cf66b58526c59aa93b87
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:09:44 +0530

    Update Child.js

--- src/pages/policy/Child.js
+++ src/pages/policy/Child.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Child = () => {
   return (
-    <div>Child</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Child Policy</h2>
+    </div>
   )
 }
 
