818e77fbb56d760bd6518df0a62c2b146e01ffb6
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:13:49 +0530

    Update Funding.js

--- src/NGOdashboard/Funding.js
+++ src/NGOdashboard/Funding.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Funding = () => {
   return (
-    <div>Funding</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Funding</h2>
+    </div>
   )
 }
 
