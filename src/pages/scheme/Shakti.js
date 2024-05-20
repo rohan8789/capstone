2ac44691a0c0df01f86f108be6fc5ea22fd82c2c
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:08:40 +0530

    Update Shakti.js

--- src/pages/scheme/Shakti.js
+++ src/pages/scheme/Shakti.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Shakti = () => {
   return (
-    <div>Shakti</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Mission Shakti</h2>
+    </div>
   )
 }
 
