7c85273f1412749dde7eceb09fcbe1c7801dc9a4
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:13:23 +0530

    Update Criteria.js

--- src/NGOdashboard/Criteria.js
+++ src/NGOdashboard/Criteria.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Criteria = () => {
   return (
-    <div>Criteria</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Criteria</h2>
+    </div>
   )
 }
 
