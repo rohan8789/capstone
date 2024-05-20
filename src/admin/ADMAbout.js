88c1cb89916dbe4052616d5fbbf37f8d3d47b93a
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:36:15 +0530

    Update ADMAbout.js

--- src/admin/ADMAbout.js
+++ src/admin/ADMAbout.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ADMAbout = () => {
   return (
-    <div>Admin about</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">
+        Admin about
+      </h2>
+    </div>
   )
 }
 
