75391b635b0303451d4d395cca1564ceb1516827
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:10:19 +0530

    Update Policy.js

--- src/pages/policy/Policy.js
+++ src/pages/policy/Policy.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Policy = () => {
   return (
-    <div>Policy</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">All policies</h2>
+    </div>
   )
 }
 
