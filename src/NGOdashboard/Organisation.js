e89922992b247fa8a3e62d7b68194ea44cefb3b5
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:14:34 +0530

    Update Organisation.js

--- src/NGOdashboard/Organisation.js
+++ src/NGOdashboard/Organisation.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Organisation = () => {
   return (
-    <div>Organisation</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Organisation</h2>
+    </div>
   )
 }
 
