0dd6219664e64e61e462d3ffed0a4e421dc571ac
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-20 09:08:11 +0530

    Update Poshan.js

--- src/pages/scheme/Poshan.js
+++ src/pages/scheme/Poshan.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Poshan = () => {
   return (
-    <div>Poshan</div>
+    <div className="container d-flex justify-content-center border mt-5">
+      <h2 className="h2">Mission Poshan</h2>
+    </div>
   )
 }
 
