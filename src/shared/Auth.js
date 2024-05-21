eb2be9ee6698d3675aac9a780e4235586bf31b9f
rohan8789 <74501400+rohan8789@users.noreply.github.com>
2024-05-21 11:29:54 +0530

    Update Auth.js

--- src/shared/Auth.js
+++ src/shared/Auth.js
@@ -5,11 +5,11 @@ import Registration from './Registration'
 const Auth = () => {
   return (
     <>
-        <div className='container-x border border-black ms-4 me-4 row border d-flex justify-content-center'>
-            <div className='col  border border-black'>
+        <div className='container-x  ms-4 me-4 row border d-flex justify-content-center'>
+            <div className='col  border'>
                 <Login/>
             </div>
-            <div className='col border border-black '>
+            <div className='col border '>
                 <Registration/>
             </div>
         </div>
